8ba0766d995bce7dd4eb922f066bb0cb55cfa76a
AmeOnna-cell <linanthony554@gmail.com>
2025-08-30 14:24:07 +0800

    Settings

--- back-in-biskits/Screens/title_screen.gd
+++ back-in-biskits/Screens/title_screen.gd
@@ -1,9 +1,11 @@
 extends Control
 
+@onready var main_buttons = $VBoxContainer
 @onready var start_button = $VBoxContainer/StartButton
 @onready var instructions_button = $VBoxContainer/InstructionsButton
 @onready var settings_button = $VBoxContainer/SettingsButton
 @onready var quit_button = $VBoxContainer/QuitButton
+@onready var options = $SettingsPanel
 
 func _ready():
 	# Connect button signals
@@ -12,42 +14,35 @@ func _ready():
 	settings_button.pressed.connect(_on_settings_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
 
+	# hide settings panel at start
+	main_buttons.visible = true
+	options.visible = false
+
 func _on_start_pressed():
 	Global.potency = 1
 	Global.timer = 0
 	FadeManager.fade_out_then_change_scene("res://Areas/tutorial.tscn")
 
 func _on_instructions_pressed():
-	# Show instructions dialog
 	show_instructions()
 
 func _on_settings_pressed():
-	# Show settings dialog
-	show_settings()
-
+	print("Settings Pressed")
+	main_buttons.visible = false
+	options.visible = true
+	#show_settings() 
 
 func _on_quit_pressed():
-	# Quit the game
 	get_tree().quit()
 
-
 func show_instructions():
-	# Create a simple instructions dialog
 	var dialog = AcceptDialog.new()
 	dialog.title = "Instructions"
-	dialog.dialog_text = """
-	Wait lang children
-	"""
+	dialog.dialog_text = "Wait lang children"
 	add_child(dialog)
 	dialog.popup_centered()
 
-func show_settings():
-	# Create a simple settings dialog
-	var dialog = AcceptDialog.new()
-	dialog.title = "Settings"
-	dialog.dialog_text = """
-	Settings
-	Lol YOU THOUGHT-????, 
-	"""
-	add_child(dialog)
-	dialog.popup_centered() 
+#func show_settings():
+
+func _on_back_options_pressed():
+	_ready()
