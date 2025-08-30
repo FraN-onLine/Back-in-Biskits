d2a8795978233ba504bc1c88d3bfb036666c358c
Franaffel <franksbass19@gmail.com>
2025-08-31 00:38:54 +0800

    candy minion

--- back-in-biskits/Bosses/Boss2/candy_minion.gd
+++ back-in-biskits/Bosses/Boss2/candy_minion.gd
@@ -4,7 +4,7 @@ class_name CandyMinion
 @export var speed: float = 100.0
 @export var max_hp: int = 50
 @export var aoe_damage: int = 1
-@export var aoe_radius: float = 80.0
+@export var aoe_radius: float = 40.0
 @export var attack_interval: float = 2.0
 
 var hp: int
@@ -43,11 +43,14 @@ func _on_attack_timeout() -> void:
 	# check if player is in AoE circle
 	if global_position.distance_to(player.global_position) <= aoe_radius:
 		print("💥 Minion AoE hit player!")
+		#wait 0.5 seconds to sync with animation
+		$AnimatedSprite2D.play("attack")
+		await get_tree().create_timer(0.6).timeout
+		
+		
 		player.call("take_damage", aoe_damage)
 
-	# optional: play attack animation
-	if has_node("AnimatedSprite2D"):
-		$AnimatedSprite2D.play("attack")
+
 
 
 # ---------------- DAMAGE ----------------
@@ -55,6 +58,9 @@ func take_damage(amount: int) -> void:
 	if not alive:
 		return
 	hp -= amount
+	$AnimatedSprite2D.modulate = Color(1, 0.5, 0.5) # flash red
+	await get_tree().create_timer(0.2).timeout
+	$AnimatedSprite2D.modulate = Color(1, 1, 1)
 	print("Minion HP: %d" % hp)
 
 	if hp <= 0:
@@ -63,7 +69,4 @@ func take_damage(amount: int) -> void:
 
 func die() -> void:
 	alive = false
-	if has_node("AnimatedSprite2D"):
-		$AnimatedSprite2D.play("death")
-		await $AnimatedSprite2D.animation_finished
 	queue_free()
