18621c1d15c8273192ea6143cea442c37e195369
AmeOnna-cell <linanthony554@gmail.com>
2025-08-31 11:46:43 +0800

    death screen i think

--- back-in-biskits/Screens/death_screen.gd
+++ back-in-biskits/Screens/death_screen.gd
@@ -3,10 +3,20 @@ extends Control
 @onready var menu = $Menu
 @onready var retry = $Retry
 
-
 func _ready():
 	menu.pressed.connect(_on_menu_pressed)
 	retry.pressed.connect(_on_retry_pressed)
+	match Global.stage:
+		1:
+			$boss_1/Label.text = "Eat my cat shit bitch, 1 life is more than enough for a motherfucker like u HAHAHAHAHAHA 😂🫵"
+			$boss_1/Sprite2D.texture = preload("res://Assets/Bosses/Cat Boss/Cat_Win.png")
+		2:
+			$boss_2/Label.text = "lady mf"
+			$boss_2/Sprite2D.texture = preload("res://Assets/Bosses/Cat Boss/cat_atk.png")
+		3:
+			pass
+		_:
+			pass
 
 func _on_menu_pressed():
 	get_tree().change_scene_to_file("res://Screens/title_screen.tscn")
